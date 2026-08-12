65b42b91314d41e66890db628dd62209f234a2df
Abu Bakkar Siddik Rifat <125892159+Rifat-siddik@users.noreply.github.com>
2026-08-12 23:59:11 +0600

    Update main.cpp
    cpp code

--- 01-oop-basics/main.cpp
+++ 01-oop-basics/main.cpp
@@ -1,1 +1,39 @@
+// Procedural vs OOP 
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Procedural
+void rifat(string name, int roll) {
+    cout << "Procedural use Name: " << name << ", Roll: " << roll << endl;
+}
+
+
+// ২. OOP
+class Student {
+public:
+    string name;
+    int roll;
+
+    void printInfo() {
+        cout << "OOP use Name: " << name << ", Roll: " << roll << endl;
+    }
+};
+
+
+int main() {
+    
+    // Procedural use
+    string studentName = "Rafi";
+    int studentRoll = 101;
+    rifat(studentName, studentRoll); 
+
+    // OOP use
+    Student s1;
+    s1.name = "Rafi";
+    s1.roll = 101;
+    s1.printInfo(); 
+
+   
+}
